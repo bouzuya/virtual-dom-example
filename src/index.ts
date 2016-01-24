9055d95b006ca35a1c1e4e710bbcac65d9cad896
bouzuya <m@bouzuya.net>
2016-01-25 08:30:24 +0900

    initial implementation

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import h from 'virtual-dom/h';
+import diff from 'virtual-dom/diff';
+import patch from 'virtual-dom/patch';
+import createElement from 'virtual-dom/create-element';
+
+type State = {
+  count: number;
+  tree: VirtualDOM.VNode;
+  rootNode: Element;
+};
+
+const render = (count: number) => {
+  return h('div', {
+    style: {
+      textAlign: 'center',
+      lineHeight: (100 + count) + 'px', 
+      border: '1px solid red',
+      width: (100 + count) + 'px',
+      height: (100 + count) + 'px'
+    }
+  }, [String(count)]);
+};
+
+const update = ({ count, tree, rootNode }: State): State => {
+  const newCount = count + 1;
+  const newTree = render(newCount);
+  const patches = diff(tree, newTree);
+  const newRootNode = patch(rootNode, patches);
+  return { count: newCount, tree: newTree, rootNode: newRootNode };
+};
+
+export default function main() {
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('load');
+    const count = 0;
+    const tree = render(count);
+    const rootNode = createElement(tree);
+    document.body.appendChild(rootNode);
+    let state = { count, tree, rootNode };
+    setInterval(() => { state = update(state); }, 1000);
+  });
+}
